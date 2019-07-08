ad653a382e24544040a794092d22514bbe279211
Michael Dales <michael@digitalflapjack.com>
2019-07-08 12:49:36 +0100

    Move header summary code.

---
+++ tests/test_header_summary.py
@@ -0,0 +1,16 @@
+import unittest
+
+from forestplots import SPSSForestPlot
+
+class DecodeExampleSPSSForestPlotHeaderSummary(unittest.TestCase):
+
+    def test_example_1(self):
+        example = 'Odds Ratio\nM-H. Fixed. 95% Cl\n\x0c'
+        values = SPSSForestPlot._decode_header_summary_ocr(example)
+        self.assertEqual(values, ("M-H", "Fixed", "95"))
+
+    def test_example_3(self):
+        example = 'Mean Difference\nIV. Random. 95% Cl\n\x0c'
+        values = SPSSForestPlot._decode_header_summary_ocr(example)
+        self.assertEqual(values, ("IV", "Random", "95"))
+
